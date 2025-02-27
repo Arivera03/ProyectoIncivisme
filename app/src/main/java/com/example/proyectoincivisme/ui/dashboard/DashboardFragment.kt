e2ddb660da6159cbc02d7d4f8154f90ec0e38f02
alvaro <alvaroriveravivero@gmail.com>
2025-02-27 21:52:57 +0100

    va hasta lo de las fotos

--- app/src/main/java/com/example/proyectoincivisme/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/proyectoincivisme/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,23 @@
 package com.example.proyectoincivisme.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectoincivisme.databinding.FragmentDashboardBinding
+import com.example.proyectoincivisme.ui.Pedido
+import com.example.proyectoincivisme.ui.home.HomeViewModel
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.example.proyectoincivisme.databinding.RecyclerviewBinding
 
 class DashboardFragment : Fragment() {
 
@@ -17,20 +27,34 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    lateinit var authUser: FirebaseUser
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val sharedViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        sharedViewModel.getUser().observe(viewLifecycleOwner) { user ->
+            authUser = user as FirebaseUser
+            val base = FirebaseDatabase.getInstance("https://proyectoincivisme-default-rtdb.europe-west1.firebasedatabase.app").reference
+            val users = base.child("users")
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            val uid = users.child(authUser.uid)
+            val pedidos = uid.child("pedidos")
+
+            val options = FirebaseRecyclerOptions.Builder<Pedido>()
+                .setQuery(pedidos, Pedido::class.java)
+                .setLifecycleOwner(viewLifecycleOwner)
+                .build()
+
+            val adapter = PedidoAdapter(options)
+
+            binding.rvPedidos.adapter = adapter
+            binding.rvPedidos.layoutManager = LinearLayoutManager(requireContext())
         }
         return root
     }
@@ -38,5 +62,27 @@ class DashboardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    class PedidoAdapter(options: FirebaseRecyclerOptions<Pedido>): FirebaseRecyclerAdapter<Pedido, PedidoAdapter.PedidoHolder>(options) {
+
+        override fun onBindViewHolder(
+            holder: PedidoHolder,
+            position: Int,
+            model: Pedido
+        ) {
+            holder.binding.txtDescripcion.text = model.motivoPedido
+            holder.binding.txtDireccion.text = model.direccion
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PedidoHolder {
+            val binding = RecyclerviewBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+            return PedidoHolder(binding)
+        }
+
+        class PedidoHolder(val binding: RecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
+
     }
 }
